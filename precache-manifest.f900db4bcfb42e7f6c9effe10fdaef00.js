d26682f3d5fe2149a17a909776f0e380e39f20c2
Slava <slava91petrushin@yandex.ru>
2019-11-04 21:15:22 +0300

    Updates

---
+++ precache-manifest.f900db4bcfb42e7f6c9effe10fdaef00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26bafc8d93bf871b0d4aff660e3fc181",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6a4d3e0fb21200e3cc4e",
+    "url": "/portfolio/static/css/main.25f8ed2b.chunk.css"
+  },
+  {
+    "revision": "3a35cd6e39a94ad9bef3",
+    "url": "/portfolio/static/js/2.f3d63cb5.chunk.js"
+  },
+  {
+    "revision": "6a4d3e0fb21200e3cc4e",
+    "url": "/portfolio/static/js/main.b099284a.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
