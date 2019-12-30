8320def073af1315935a1c5c39162d728803601d
Slava <slava91petrushin@yandex.ru>
2019-12-30 13:32:22 +0300

    bg url for project

--- src/conponents/Projects/Project/Project.js
+++ src/conponents/Projects/Project/Project.js
@@ -2,23 +2,26 @@ import React from 'react';
 import classes from './Project.module.css';
 import Button from '../../Button/Button';
 
-
 const Project = (props) => {
 	const divStyle = {
-		'position': 'absolute',
-		'top': `50%`,
-		'left': `50%`,
-		'transform': `translate(-50%, -50%)`
+		btn : {
+			'position': 'absolute',
+			'top': `50%`,
+			'left': `50%`,
+			'transform': `translate(-50%, -50%)`
+		},
+		background : {
+			'backgroundImage': `url('${props.projectInfo.url}')`
+		}
 	}
-
 	return (
 		<div className={classes.projectsWork}>
-			<div className={classes.projectsImg}>
-				<Button title={"Смотреть"} style={divStyle}/>
+			<div className={classes.projectsImg} style={divStyle.background}>
+				<Button title={"Смотреть"} style={divStyle.btn}/>
 			</div>
 			<div className={classes.projectsDescription}>
-				<p>Название проекта</p>
-				<p>Краткое описание</p>
+				<p>{props.projectInfo.title}</p>
+				<p>{props.projectInfo.description}</p>
 			</div>
 		</div>			
 	)
--- src/conponents/Projects/Projects.js
+++ src/conponents/Projects/Projects.js
@@ -2,16 +2,22 @@ import React from 'react';
 import classes from './Projects.module.css';
 import Project from './Project/Project';
 
+const projects = [
+	{title : "Burger shop", description : "A shop selling burgers, senior project LoftSchoool", url : "https://www.beingeorge.com/jeffrey/assets/images/p1-1.jpg" },
+	{title : "Burger shop", description : "A shop selling burgers, senior project LoftSchoool", url : "https://www.beingeorge.com/jeffrey/assets/images/p1-1.jpg" },
+	{title : "Burger shop", description : "A shop selling burgers, senior project LoftSchoool", url : "https://www.beingeorge.com/jeffrey/assets/images/p1-1.jpg" },
+	{title : "Burger shop", description : "A shop selling burgers, senior project LoftSchoool", url : "https://www.beingeorge.com/jeffrey/assets/images/p1-1.jpg" },			
+]
+
 function Projects() {
   return (
     <div className={classes.projects}>
 			<div className={classes.projectsBlock}>
 				<h2 className={classes.projectsTitle}>Мои Работы</h2>					
 				<div className={classes.projectsWorks}>
-					<Project />
-					<Project />
-					<Project />
-					<Project />					
+					{
+						projects.map(project => <Project projectInfo={project}/>)
+					}
 				</div>
 			</div>
     </div>
