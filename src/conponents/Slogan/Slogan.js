8b4a5f7eec757291a737724ac3cc74a579e74061
Slava <slava91petrushin@yandex.ru>
2020-01-11 19:55:46 +0300

    Create TitleBlock, style: Slogan, Project, Title, Skils

--- src/conponents/Slogan/Slogan.js
+++ src/conponents/Slogan/Slogan.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './Slogan.module.css';
 import Button from '../Button/Button';
+import TitleBlock from '../commonBlocks/TitleBlock/TitleBlock';
 
 function Slogan() {
   return (
     <div className={classes.slogan}>
 			<div className={classes.sloganBlock}>
-				<h2 className={classes.sloganTitle}>Рассматриваю варианты удаленной рабты</h2>
+				<TitleBlock title={"Рассматриваю варианты удаленной рабты"}/>
 				<Button title={"Нанять меня"}/>
 			</div>
     </div>
